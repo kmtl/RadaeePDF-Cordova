1263dba1824848995bc33db5b97b314dffc8ed99
Kita Hiroyuki <kita_hryk@actis.co.jp>
2017-04-12 15:34:06 +0900

    Added filepath to success plugin install on my environment

--- src/android/hooks/configure-android.js
+++ src/android/hooks/configure-android.js
@@ -45,6 +45,8 @@ module.exports = function (ctx) {
     	{filePath: 'src/com/radaee/util/PDFGridItem.java', importStatement: 'com.radaee.viewlib.R'},
 		{filePath: 'src/com/radaee/util/PopupEditAct.java', importStatement: 'com.radaee.viewlib.R'},
 		{filePath: 'src/com/radaee/util/CommonUtil.java', importStatement: 'com.radaee.viewlib.R'}
+		{filePath: 'src/com/radaee/util/RadaeePDFManager.java', importStatement: 'com.radaee.reader.R'},
+		{filePath: 'src/com/radaee/reader/PDFPagerAct.java', importStatement: 'com.radaee.reader.R'}
     ];
 
 
